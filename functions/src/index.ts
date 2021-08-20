b3261f04b47db613024095914d6401f7b7219e51
pannacotta98 <algot.sandahl@gmail.com>
2021-08-20 11:45:40 +0200

    Add emergency billing kill function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
-import * as functions from 'firebase-functions';
-import { createInvite, acceptInvite } from './invites';
+// import * as functions from 'firebase-functions';
+// import { createInvite, acceptInvite } from './invites';
+import { capBilling } from './billingEmergencyKill';
 
-export { createInvite, acceptInvite };
+export { capBilling };
 
-export const testCallableFunction = functions.https.onCall(async (idk, context) => {
-  return { test1: 'hej', test2: 'hej2', mail: context.auth?.token.email };
-});
+// export { createInvite, acceptInvite };
+
+// export const testCallableFunction = functions.https.onCall(async (idk, context) => {
+//   return { test1: 'hej', test2: 'hej2', mail: context.auth?.token.email };
+// });
