ef98086cc0221a65ffd0f886e7f4f0ed17e5f88e
Algot Sandahl <algot.sandahl@gmail.com>
2021-08-22 18:26:38 +0200

    Add the very basics to recieve messages

--- src/other/firebase.ts
+++ src/other/firebase.ts
@@ -3,15 +3,43 @@ import 'firebase/analytics';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/functions';
-import firebaseConfig from '../firebaseConfig';
+import 'firebase/messaging';
+import { firebaseConfig, vapidPushKey } from '../firebaseConfig';
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 } else {
   firebase.app(); // if already initialized, use that one
 }
+
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const messaging = firebase.messaging();
+
+messaging
+  .getToken({
+    vapidKey: vapidPushKey,
+  })
+  .then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      // ...
+      console.log('Token: ', currentToken);
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+    }
+  })
+  .catch((err) => {
+    console.log('An error occurred while retrieving messaging token. ', err);
+    // ...
+  });
+
+messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
 
 // // DEBUG bipediboop
 // firebase.functions().useEmulator('localhost', 5001);
